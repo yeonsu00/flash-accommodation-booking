9a5c435438965856ad9e8428d5f8f936ca705769
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2026-06-09 22:08:34 +0900

    Initial commit

---
+++ src/main/java/com/flashaccommodationbooking/FlashAccommodationBookingApplication.java
@@ -0,0 +1,13 @@
+package com.flashaccommodationbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashAccommodationBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashAccommodationBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flashaccommodationbooking/FlashAccommodationBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashaccommodationbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashAccommodationBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
