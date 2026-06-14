384df1470b16d3d6991570bed2741ee22c6137b1
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2026-06-14 14:26:00 +0900

    feat: 필수 헤더 누락 예외 처리 추가

--- src/main/java/com/flashaccommodationbooking/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/flashaccommodationbooking/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.flashaccommodationbooking.global.common.CommonApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,6 +31,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(400)
                 .body(CommonApiResponse.fail(ErrorCode.BAD_REQUEST, message));
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<CommonApiResponse<Void>> handleMissingRequestHeaderException(
+            MissingRequestHeaderException e
+    ) {
+        return ResponseEntity
+                .status(400)
+                .body(CommonApiResponse.fail(ErrorCode.BAD_REQUEST, e.getHeaderName() + ": 필수 헤더입니다."));
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
